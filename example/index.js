37a4adfc26b4047fe17029645bf7b6c294e0b539
PRLsir <18824673092@163.com>
2023-07-18 22:18:29 +0800

    demo环境升级

--- example/index.js
+++ example/index.js
@@ -7,7 +7,7 @@ import CustomView1 from './CustomView1';
 import CustomView2 from './CustomView2';
 import CustomView3 from './CustomView3';
 
-AppRegistry.registerComponent('example', () => App);
+AppRegistry.registerComponent('JVRNTest', () => App);
 AppRegistry.registerComponent('customView1', () => CustomView1);
 AppRegistry.registerComponent('customView2', () => CustomView2);
 AppRegistry.registerComponent('customView3', () => CustomView3);
